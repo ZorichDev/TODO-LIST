ec31bb2b998d9007bbded3443afd6ecd748c2a40
Franzor <ffranzor@yahoo.com>
2026-05-13 15:17:10 +0100

    app . get

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // MongoDB Connection
 const Live_url =
   "mongodb://Franzor:Franzor2929@ac-qilthkv-shard-00-00.cgpgoxc.mongodb.net:27017,ac-qilthkv-shard-00-01.cgpgoxc.mongodb.net:27017,ac-qilthkv-shard-00-02.cgpgoxc.mongodb.net:27017/tododb?ssl=true&replicaSet=atlas-svachj-shard-0&authSource=admin&retryWrites=true&w=majority";
-     
+
 mongoose
   .connect(Live_url)
   .then(() => console.log("Connected to MongoDB"))
@@ -23,6 +23,10 @@ mongoose
 
 // Routes
 app.use("/api/todos", todoRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Todo API");
+});
 
 // Server
 app.listen(PORT, () => {
